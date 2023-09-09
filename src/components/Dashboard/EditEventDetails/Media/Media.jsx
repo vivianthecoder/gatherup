475047736f1a8c897e0ded4b93ce9447a34dfaa4
vivianthecoder <vivianlamyu@gmail.com>
2023-09-09 14:47:00 -0400

    Edited image carousel styling

--- src/components/Dashboard/EditEventDetails/Media/Media.jsx
+++ src/components/Dashboard/EditEventDetails/Media/Media.jsx
@@ -4,6 +4,15 @@ import ImageCarousel from './ImageCarousel';
 
 const Media = () => {
     
+    const images = [
+        "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ];
+    
     return(
         <div className='edit-event-details'>
             <div className='sub-header'>
@@ -17,7 +26,7 @@ const Media = () => {
             </div>
 
             <h3>Photos</h3>
-            <ImageCarousel />
+            <ImageCarousel images={images} />
 
             <form>
                 <input type="file" id="img" name="img" accept="image/*"/>
