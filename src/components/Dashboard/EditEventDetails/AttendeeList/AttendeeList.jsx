72d959ae55a36b4781a866f799251500cd3edc16
vivianthecoder <vivianlamyu@gmail.com>
2023-09-09 23:29:19 -0400

    Added styling to attendee list

--- src/components/Dashboard/EditEventDetails/AttendeeList/AttendeeList.jsx
+++ src/components/Dashboard/EditEventDetails/AttendeeList/AttendeeList.jsx
@@ -2,41 +2,62 @@ import './AttendeeList.scss'
 import NewAttendeeForm from './NewAttendeeForm';
 import GroupIcon from '../../../../assets/Icons/Group.svg';
 import AddEventIcon from '../../../../assets/Icons/Add_square.svg';
+import axios from 'axios';
 import { useState } from 'react';
+import { useParams } from 'react-router';
+import { useEffect } from 'react';
 
 const AttendeeList = () => {
-    const [isInviteFormVisible, setInviteFormVisible] = useState(false);
-    const [inviteEmail, setInviteEmail] = useState('');
-    const [showForm, setShowForm] = useState(false);
-    const [name, setName] = useState('');
-    // Array to store emails
-    const [rsvpList, setRsvpList] = useState([]);
+    const [attendeeList, setAttendeeList] = useState([]);
+    const [isInviteFormOpen, setInviteFormOpen] = useState(false);
+    const { id } = useParams();
 
-    const handleAddAttendeeClick = () => {
-        setInviteFormVisible(true);
-    };
-
-    const handleEmailChange = (e) => {
-        setInviteEmail(e.target.value);
-    };
-
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    };
-
-    const handleSendInvitation = () => {
-        setRsvpList([...rsvpList, inviteEmail]);
-        console.log('Sending invitation to:', inviteEmail);
-        setInviteEmail('');
-        setInviteFormVisible(false);
+    // To get event data from all events from the server
+    function eventListRequest() {
+        axios
+            .get(`http://localhost:3031/dashboard`)
+            .then(response => {
+                setAttendeeList(response.data)
+            }).catch((error) => console.log('Failed fetching data', error)
+        );
     }
-    const handleAddAttendee = () => {
-        setRsvpList([...rsvpList, `${name} (${inviteEmail})`])
-        setName('');
-        setInviteEmail('');
-        setShowForm(false);
+
+    const handleUpdateAttendeeList = (eventId, newAttendee) => {
+        setAttendeeList(attendees => {
+            return attendees.map(event => {
+                if (event.id === eventId) {
+                    return {
+                        ...event,
+                        attendees: [...AddEventIcon(event.attendees || []), newAttendee]
+                    }
+                }
+                return event;
+            })
+        })
+    };
+
+    // To display invite form after clicking into box
+    const handleAddAttendeeClick = () => {
+        setInviteFormOpen(true);
+    };
+
+    // const handleSendInvitation = () => {
+    //     setRsvpList([...rsvpList, inviteEmail]);
+    //     console.log('Sending invitation to:', inviteEmail);
+    //     setInviteEmail('');
+    //     setInviteFormOpen(false);
+    // }
+  
+    // To push the newEvent object onto the setEvents array with updated fields WORKS!
+    const addAttendees = (newAttendee) => {
+        setAttendeeList([...attendeeList, newAttendee])
     };
     
+    // To fetch list of all events from the server
+    useEffect(() => {
+        eventListRequest();
+    }, []);
+
     return(
         <div className='edit-event-details'>
             <div className='sub-header'>
@@ -50,24 +71,26 @@ const AttendeeList = () => {
             </div>
             
             <div className='add-attendee-box'>
-                <button className='first-add-btn' onClick={() => setShowForm(true)}>
+                <button className='first-add-btn' onClick={() => setInviteFormOpen(true)}>
                     <img src={AddEventIcon} alt='Add Event' className='add-btn' />
                     <h3>Add Attendee</h3>
                 </button>
-                {showForm && 
+                {isInviteFormOpen && 
                     <NewAttendeeForm 
-                        addAttendee={addAttendee} 
-                        setShowForm={setShowForm} 
+                        setInviteFormOpen={setInviteFormOpen}
+                        addAttendees={addAttendees}
+                        handleUpdateAttendeeList={handleUpdateAttendeeList}
+                        eventId={id}
                     />
                 }
             </div>
 
             <h3>RSVP List</h3>
-            <ul>
-                {rsvpList.map((attendee, index) => (
+            <ul className='attendee-list'>
+                {attendeeList.map((attendee, index) => (
                     <li key={index}>
-                    Name: {attendee.name}
-                    Email: {attendee.email}
+                    {attendee.attendeeName && <span>Name: {attendee.attendeeName}</span>}
+                    {attendee.attendeeName && <span>Email: {attendee.attendeeEmail}</span>}
                     </li>
                 ))}
             </ul>
--- src/components/Dashboard/EditEventDetails/AttendeeList/NewAttendeeForm.jsx
+++ src/components/Dashboard/EditEventDetails/AttendeeList/NewAttendeeForm.jsx
@@ -1,37 +1,79 @@
-const NewAttendeeForm = ({ addAttendee, setShowForm}) => {
-    // To create a new event object
-    const [event, setEvent] = useState({
-        attendeeList: []
+import { useState } from "react";
+import axios from "axios";
+
+const NewAttendeeForm = ({ addAttendees, setInviteFormOpen}) => {
+    // To create a new attendee list object
+    const [list, setList] = useState({
+        attendeeName: '',
+        attendeeEmail:''
     });
 
-
-    // To handle changes in form fields and update the event object
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setEvent({
-            ...event,
-            [name]: value   
-        });
+    const handleNameChange = (e) => {
+        const { name, value } = e.target.value;
+        setList({
+            ...list,
+            [name]: value
+        })
     };
+    const handleEmailChange = (e) => {
+        const { name, value } = e.target.value;
+        setList({
+            ...list,
+            [name]: value
+        })
+    };
+
+    // To handle the submit button clicked in the new attendee form field
+    const handleSubmit = () => {
+        // To check if all form fields are filled
+        if (
+            list.attendeeName &&
+            list.attendeeEmail
+        ) {
+            // To post the data to our server data
+            // axios
+            //     .post(`http://localhost:3031/dashboard`, list, {
+            //         headers: {
+            //             'Content-Type' : 'application/json',
+            //         },
+            //     })
+            //     .then(response => {
+            //         console.log('Success', response.data);
+            //         addAttendees(response.data);
+
+            //         setList({
+            //             attendeeList: ''
+            //         });
+            //         // To close the form
+            //         setInviteFormOpen(false);
+            //     })
+            //     .catch(error => {
+            //         console.error('Error',  error);
+            //     })
+        }
+    };
+    
 
     return(
         <div className='overlay'>
         <div className='event-form'>
-            <button className='exit-btn' onClick={() => setShowForm(false)}>X</button>
+            <button className='exit-btn' onClick={() => setInviteFormOpen(false)}>X</button>
             <h2>Enter Form Fields</h2>
             <input
                 type='text'
-                placeholder='Event Name'
-                name='eventName'
-                value={event.eventName}
-                onChange={handleChange}
+                placeholder='Name'
+                name='attendeeName'
+                value={list.attendeeEmail}
+                onChange={handleNameChange}
             />
             <input  
-                type='date'
-                name='eventDate'
-                value={event.eventDate}
-                onChange={handleChange}
+                type='text'
+                placeholder='Email'
+                name='attendeeEmail'
+                value={list.attendeeEmail}
+                onChange={handleEmailChange}
             />
+            <button className='submit-btn' onClick={handleSubmit}>Save and Continue</button>
             <button className='submit-btn' onClick={handleSubmit}>Submit</button>
         </div>
     </div>
