b8c8de002ea7a9cd4eb66fbb57a6916fd59ac136
vivianthecoder <vivianlamyu@gmail.com>
2023-09-10 14:39:24 -0400

    Added functionality to print box component

--- src/pages/NotFound404Page/NotFound404Page.jsx
+++ src/pages/NotFound404Page/NotFound404Page.jsx
@@ -6,7 +6,10 @@ const NotFound404 = () => {
             <h1>404</h1>
             <p>Can't find what you're looking for, sorry!</p>
             <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC9U7hwo8FAcfXhQa-VCQKbJByZwT3PcRzFtsGTEM4v4y-YFZVUmp6D1vr5uUPLMlHMow&usqp=CAU" />
+                <img 
+                alt="Work In Progress Chicken"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC9U7hwo8FAcfXhQa-VCQKbJByZwT3PcRzFtsGTEM4v4y-YFZVUmp6D1vr5uUPLMlHMow&usqp=CAU" 
+                />
             </div>
         </div>
     )
