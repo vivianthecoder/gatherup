b7e34073910635fbdd22fad49f0bd6035ac42e0f
vivianthecoder <vivianlamyu@gmail.com>
2023-09-10 14:09:53 -0400

    Added images to Notifications, Settings, and NotFound page

--- src/pages/NotFound404Page/NotFound404Page.jsx
+++ src/pages/NotFound404Page/NotFound404Page.jsx
@@ -5,6 +5,9 @@ const NotFound404 = () => {
         <div className='not-found-content'>
             <h1>404</h1>
             <p>Can't find what you're looking for, sorry!</p>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC9U7hwo8FAcfXhQa-VCQKbJByZwT3PcRzFtsGTEM4v4y-YFZVUmp6D1vr5uUPLMlHMow&usqp=CAU" />
+            </div>
         </div>
     )
 }
